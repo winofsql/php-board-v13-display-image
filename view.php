8d1836d4d57265f17d133f88936957283b6c66c2
sworc <lightbox@nifty.com>
2022-06-16 15:43:24 +0900

    Add files via upload

--- view.php
+++ view.php
@@ -4,7 +4,7 @@
     <meta content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
-    <title>掲示板</title>
+    <title>掲示板 v13 画像表示</title>
     <link rel="stylesheet" href="client.css?_=<?= time() ?>">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.css">
@@ -106,7 +106,6 @@ $( function(){
             $("#text").val("");
             $("#id").val("");
 
-
             $("#file").val("");
             $("#upload").prop("disabled", true);
 
@@ -135,28 +134,46 @@ $( function(){
 <body>
 <div id="bbs">
     <h3 class="alert alert-primary"><a href="control.php" style="color:black;">掲示板 ( MySQL )</a></h3>
-    <div id="content">
-    <form method="POST" target="myframe">
-        <div>
-            <span class="title_entry">タイトル</span> 
-            <input type="text" name="subject" id="subject" pattern=".*\S+.*" required>
-        </div>
-        <div>
-            <span class="title_entry">名前</span>
-            <input type="text" name="name" id="name" pattern="[ぁ-んァ-ン一-龥 　]+" required>
-        </div>
-        <div>
-            <textarea name="text" id="text"></textarea>
-        </div>
-        <div>
-            <input type="submit" name="send" id="send" value="送信">
-            <input type="file" name="file" id="file" accept=".jpg,.jpeg">
-            <input type="button" id="upload" value="画像アップロード" disabled>
-        </div>
-        <input type="hidden" name="datetime" id="datetime">
-
-        <input type="hidden" name="id" id="id">
-    </form>
+    <div id="content"
+        >
+        <form action=""
+            target="myframe"
+            method="POST">
+            <div>
+                <span class="title_entry">
+                    タイトル
+                </span>
+                <input
+                    type="text"
+                    name="subject"
+                    id="subject"
+                    pattern=".*\S+.*"
+                    required
+                    >
+            </div>
+            <div>
+                <span class="title_entry">
+                    名前
+                </span>
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    pattern="[ぁ-んァ-ン一-龥 　]+"
+                    required
+                    >
+            </div>
+            <div>
+                <textarea name="text" id="text"></textarea>
+            </div>
+            <div>
+                <input type="submit" name="send" id="send" value="送信">
+                <input type="file" name="file" id="file" accept=".jpg,.jpeg">
+                <input type="button" id="upload" value="画像アップロード" disabled>
+            </div>
+            <input type="hidden" name="datetime" id="datetime">
+            <input type="hidden" name="id" id="id">
+        </form>
     </div>
 </div>
 
